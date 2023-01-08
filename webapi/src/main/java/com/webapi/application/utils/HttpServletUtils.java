5f3ac5d4e13a0bb97df09c20cdcad2830e8e0e01
Emrullah Şahin <emrullahsahin551@gmail.com>
2023-01-08 21:41:47 +0300

    Helper classes added for Servlets.

---
+++ webapi/src/main/java/com/webapi/application/utils/HttpServletUtils.java
@@ -0,0 +1,10 @@
+package com.webapi.application.utils;
+
+public final class HttpServletUtils {
+
+	private HttpServletUtils() {
+		
+	}
+
+	public static final String MEDIA_TYPE_JSON = "application/json";
+}
