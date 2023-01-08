b6f087ed61816b343a0052d6d5b1097b20d18a01
Emrullah Şahin <emrullahsahin551@gmail.com>
2023-01-08 22:18:34 +0300

    Base entity encapsulation.

--- webapi/src/main/java/com/webapi/domain/abstractions/BaseEntity.java
+++ webapi/src/main/java/com/webapi/domain/abstractions/BaseEntity.java
@@ -1,5 +1,14 @@
 package com.webapi.domain.abstractions;
 
 public abstract class BaseEntity {
-	public int id;
+
+	private int id;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 }
