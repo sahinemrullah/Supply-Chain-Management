764d18dbf178c24c369d73e5593337561ab74b1b
Emrullah Şahin <emrullahsahin551@gmail.com>
2023-01-08 21:42:30 +0300

    Supplier repository created.

---
+++ webapi/src/main/java/com/webapi/persistence/abstractions/ISupplierRepository.java
@@ -0,0 +1,7 @@
+package com.webapi.persistence.abstractions;
+
+import com.webapi.domain.entities.Supplier;
+
+public interface ISupplierRepository extends IRepository<Supplier> {
+	public boolean supplierExists(String email); 
+}
---
+++ webapi/src/main/java/com/webapi/persistence/concretes/SupplierRepository.java
@@ -0,0 +1,77 @@
+package com.webapi.persistence.concretes;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import com.webapi.application.utils.StringUtils;
+import com.webapi.domain.entities.Supplier;
+import com.webapi.persistence.DatabaseConnection;
+import com.webapi.persistence.abstractions.ISupplierRepository;
+
+public class SupplierRepository implements ISupplierRepository {
+
+	public SupplierRepository() {
+
+	}
+
+	@Override
+	public boolean add(Supplier entity) {
+		Connection con = DatabaseConnection.getConntection();
+		int rowAffected = 0;
+		try {
+			PreparedStatement statement = con.prepareStatement("INSERT INTO SUPPLIER(name, email, phone_number, password_hash) VALUES(?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+			
+			statement.setString(1, entity.getName());
+			statement.setString(2, entity.getEmail());
+			statement.setString(3, entity.getPhoneNumber());
+			statement.setString(4, entity.getPasswordHash());
+			
+			rowAffected = statement.executeUpdate();
+		} catch (SQLException e) {
+			return false;
+		}
+		
+		return rowAffected == 1;
+	}
+
+	@Override
+	public void delete(Supplier entity) {
+
+		
+	}
+
+	@Override
+	public void update(Supplier entity) {
+
+		
+	}
+
+	@Override
+	public ArrayList<Supplier> getAll() {
+
+		return null;
+	}
+
+	@Override
+	public boolean supplierExists(String email) {
+		Connection con = DatabaseConnection.getConntection();
+		try {
+			PreparedStatement statement = con.prepareStatement("SELECT email FROM SUPPLIER WHERE email = ?");
+			statement.setString(1, email);
+			if(statement.execute()) {
+				ResultSet result = statement.getResultSet();
+				if(result.next()) {
+					return StringUtils.isEqual(email, result.getString("email"));
+				}
+			}
+		} catch (SQLException e) {
+			return false;
+		}
+		return false;
+	}
+
+}
