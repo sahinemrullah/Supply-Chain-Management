9c9df7bd46639915172b4f653283b60e1941a84b
Emrullah Şahin <emrullahsahin551@gmail.com>
2023-01-22 20:42:23 +0300

    Logout page added.

---
+++ webapp/src/main/java/com/webapp/servlets/index/LogoutServlet.java
@@ -0,0 +1,23 @@
+package com.webapp.servlets.index;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet(name = "LogoutServlet", urlPatterns = {"/cikis"})
+public class LogoutServlet extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession session = request.getSession();
+        session.invalidate();
+        response.sendRedirect("/giris");
+    }
+
+}
